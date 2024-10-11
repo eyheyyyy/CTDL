4abe6871943470db8ba2ac93bb827d1c2c7638d0
nhub9889 <153304029+nhub9889@users.noreply.github.com>
2024-10-11 15:16:01 +0700

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -11,7 +11,7 @@ class Employee{
     Info getThongTin() {return thongtin;}
     void setUsername(string _username) { username = _username;}
     void setPassword (string _password) {password = _password;}
-    setThongTin(Info _thongtin) { thongtin = _thongtin;}
+    void setThongTin(Info _thongtin) { thongtin = _thongtin;}
     void Thoat() {return EXIT_SUCCESS;}
     void XemThongTin();
     void DoiPassWord(string);
