3ac9565bf45f6b77998ecc25f337e266c838d97b
nhub9889 <153304029+nhub9889@users.noreply.github.com>
2024-10-11 19:17:02 +0700

    Update Info.cpp

--- Info.cpp
+++ Info.cpp
@@ -7,7 +7,15 @@ class Info{
     string getTen() {
         return ten;
     }
-
+    string getDienThoai(){
+        return dienThoai;
+    }
+    string getDiaChi(){
+        return diaChi;
+    }
+    string getEmail(){
+        return email;
+    }
     void setThongTin(string username){
         string fn = username + ".txt";
         ifstream in (fn);
