620c6c6b7ad4b8a316669f779a992d2cf1b8b44e
nhub9889 <153304029+nhub9889@users.noreply.github.com>
2024-10-11 13:34:22 +0700

    Create Man.cpp

---
+++ Man.cpp
@@ -0,0 +1,15 @@
+
+using namespace std;
+
+class Man{
+    public : 
+    int vitriX;
+    int vitriY;
+    int dai, rong;
+    string NoiDung;
+    public :
+    void xuLySuKien();
+    void XuLyPhim(key_event_record);
+    void ThongBao();
+
+};
